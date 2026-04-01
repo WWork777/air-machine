30621a90c1defd3308a9797adc3969cb7f23b76a
id0ntc4re <parsikovevgenij470@gmail.com>
2026-04-01 10:37:49 +0700

    qwe

--- app/components/layout/Footer/Footer.tsx
+++ app/components/layout/Footer/Footer.tsx
@@ -1,123 +1,125 @@
-import styles from "./Footer.module.scss"
+import styles from "./Footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Footer(){
+export default function Footer() {
+  const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL;
+  const phone = process.env.NEXT_PUBLIC_CONTACT_PHONE;
+  const address = process.env.NEXT_PUBLIC_CONTACT_ADDRESS;
+  const addressLink = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_LINK;
 
-    const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL;
-    const phone = process.env.NEXT_PUBLIC_CONTACT_PHONE;
-    const address = process.env.NEXT_PUBLIC_CONTACT_ADDRESS;
-    const addressLink = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_LINK;
+  return (
+    <footer>
+      <div className={styles.footer}>
+        <div className={styles.section}>
+          <h4>Компания</h4>
+          <ul>
+            <li>
+              <Link href="#about">О нас</Link>
+            </li>
+            <li>
+              <Link href="#products">Продукция</Link>
+            </li>
+            <li>
+              <Link href="#services">Услуги</Link>
+            </li>
+            <li>
+              <Link href="#contacts">Контакты</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.section}>
+          <h4>Продукты</h4>
+          <ul>
+            <li>
+              <Link href="/products/ursa">АМ-1 «Урса»</Link>
+            </li>
+            <li>
+              <Link href="/products/dilin">СВП «Дилин»</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={`${styles.section} ${styles.section__contacts}`}>
+          <h4>Контакты</h4>
+          <ul>
+            <li>
+              <a className={styles.footerContacts} href={addressLink}>
+                <Image
+                  src="/svg/mapPoint.svg"
+                  alt="Адрес"
+                  width={25}
+                  height={25}
+                />
+                {address}
+              </a>
+            </li>
+            <li>
+              <a className={styles.footerContacts} href={`tel:${phone}`}>
+                <Image
+                  src="/svg/phone.svg"
+                  alt="Адрес"
+                  width={25}
+                  height={25}
+                />
+                {phone}
+              </a>
+            </li>
+            <li>
+              <a className={styles.footerContacts} href={`mailto:${email}`}>
+                <Image src="/svg/mail.svg" alt="Адрес" width={25} height={25} />
+                {email}
+              </a>
+            </li>
+          </ul>
+        </div>
 
-    return(
-        <footer>
-            <div className={styles.footer}>
-                <div className={styles.section}>
-                    <h4>Компания</h4>
-                    <ul>
-                        <li><Link href="#about">О нас</Link></li>
-                        <li><Link href="#products">Продукция</Link></li>
-                        <li><Link href="#services">Услуги</Link></li>
-                        <li><Link href="#contacts">Контакты</Link></li>
-                    </ul>
-                </div>
-                <div className={styles.section}>
-                    <h4>Продукты</h4>
-                    <ul>
-                        <li><Link href="#about">Беспилотники</Link></li>
-                        <li><Link href="#products">Беспилотники</Link></li>
-                        <li><Link href="#services">Беспилотники</Link></li>
-                        <li><Link href="#contacts">Беспилотники</Link></li>
-                    </ul>
-                </div>
-                <div className={`${styles.section} ${styles.section__contacts}`}>
-                    <h4>Контакты</h4>
-                    <ul>
-                        <li>
-                            <a className={styles.footerContacts} href={addressLink}>
-                                <Image 
-                                src="/svg/mapPoint.svg"
-                                alt="Адрес"
-                                width={25}
-                                height={25}
-                                />
-                                {address}
-                            </a>
-                        </li>
-                        <li>
-                            <a className={styles.footerContacts} href={`tel:${phone}`}>
-                                <Image 
-                                src="/svg/phone.svg"
-                                alt="Адрес"
-                                width={25}
-                                height={25}
-                                />
-                                {phone}
-                            </a>
-                        </li>
-                        <li>
-                            <a className={styles.footerContacts} href={`mailto:${email}`}>
-                                <Image 
-                                src="/svg/mail.svg"
-                                alt="Адрес"
-                                width={25}
-                                height={25}
-                                />
-                                {email}
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className={`${styles.section} ${styles.section__links}`}>
-                    <div className={styles.logos}>
-                        <Link href="/" className={styles.logo}>
-                            <Image
-                            src="/svg/logo.svg"
-                            alt="Логотип"
-                            width={90}
-                            height={90}
-                            priority
-                            />
-                            <div className={styles.logoText}>
-                            <span className="subTitle">АВИА</span>
-                            <span className="subTitle">Машина</span>
-                            </div>
-                        </Link>
-                        <Link href="https://sk.ru/" target="_blank" >
-                            <Image
-                            src="/svg/skolkovo.svg"
-                            alt="Логотип"
-                            width={230}
-                            height={90}
-                            priority
-                            />
-                        </Link>
-                    </div>
-                    <div className={styles.links}>
-                        <Link href="https://m.vk.com/tridsat_dva" target="_blank">
-                            <Image
-                                className={styles.icon}
-                                src="/svg/vkWhite.svg"
-                                alt="VK"
-                                width={40}
-                                height={40}
-                            />
-                        </Link>
-                        <Link href="https://wa.me/79029830005" target="_blank">
-                            <Image
-                                className={styles.icon}
-                                src="/svg/waWhite.svg"
-                                alt="WhatsApp"
-                                width={40}
-                                height={40}
-                            />
-                        </Link>
-
-                      </div>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className={`${styles.section} ${styles.section__links}`}>
+          <div className={styles.logos}>
+            <Link href="/" className={styles.logo}>
+              <Image
+                src="/svg/logo.svg"
+                alt="Логотип"
+                width={90}
+                height={90}
+                priority
+              />
+              <div className={styles.logoText}>
+                <span className="subTitle">АВИА</span>
+                <span className="subTitle">Машина</span>
+              </div>
+            </Link>
+            <Link href="https://sk.ru/" target="_blank">
+              <Image
+                src="/svg/skolkovo.svg"
+                alt="Логотип"
+                width={230}
+                height={90}
+                priority
+              />
+            </Link>
+          </div>
+          <div className={styles.links}>
+            {/* <Link href="https://m.vk.com/tridsat_dva" target="_blank">
+              <Image
+                className={styles.icon}
+                src="/svg/vkWhite.svg"
+                alt="VK"
+                width={40}
+                height={40}
+              />
+            </Link> */}
+            <Link href="https://wa.me/79029830005" target="_blank">
+              <Image
+                className={styles.icon}
+                src="/svg/waWhite.svg"
+                alt="WhatsApp"
+                width={40}
+                height={40}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/components/mainPage/Contacts/Contacts.tsx
+++ app/components/mainPage/Contacts/Contacts.tsx
@@ -1,75 +1,63 @@
-import styles from "./Contacts.module.scss"
-import "./Maps.scss"
+import styles from "./Contacts.module.scss";
+import "./Maps.scss";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Contacts(){
+export default function Contacts() {
+  const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL;
+  const phone = process.env.NEXT_PUBLIC_CONTACT_PHONE;
+  const address = process.env.NEXT_PUBLIC_CONTACT_ADDRESS;
+  const addressLink = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_LINK;
+  const addressBuilder = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_BUILDER;
 
-    const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL;
-    const phone = process.env.NEXT_PUBLIC_CONTACT_PHONE;
-    const address = process.env.NEXT_PUBLIC_CONTACT_ADDRESS;
-    const addressLink = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_LINK;
-    const addressBuilder = process.env.NEXT_PUBLIC_CONTACT_ADDRESS_BUILDER;
-
-    return(
-        <section className="container" id="contacts">
-            
-
-            <div className={styles.contactsContentGrid}>
-                <div className={styles.side}>
-                    <h2>Контакты</h2>
-                    <a href={addressLink} className={styles.socials}>
-                      <p className={styles.socialTitle}>Адрес</p>
-                      <p>{address}</p>
-                    </a>
-                    <a href={`tel:${phone}`} className={styles.socials}>
-                      <p className={styles.socialTitle}>Телефон</p>
-                      <p>{phone}</p>
-                    </a>
-                    <a href={`mailto:${email}`} className={styles.socials}>
-                      <p className={styles.socialTitle}>Email</p>
-                      <p>{email}</p>
-                    </a>
-                    <div className={styles.socials}>
-                      <p className={styles.socialTitle}>Соцсети</p>
-                      <div className={styles.links}>
-                        <Link href="https://m.vk.com/tridsat_dva" target="_blank">
-                            <Image
-                                className={styles.icon}
-                                src="/svg/vk.svg"
-                                alt="VK"
-                                width={40}
-                                height={40}
-                            />
-                        </Link>
-                        <Link href="https://wa.me/79029830005" target="_blank">
-                            <Image
-                                className={styles.icon}
-                                src="/svg/wa.svg"
-                                alt="WhatsApp"
-                                width={40}
-                                height={40}
-                            />
-                        </Link>
-
-                      </div>
-                    </div>
-                </div>
-
-
-            
-            <div className={styles.side}>
-
-                <div className={styles.map}>
-                    <iframe src={addressBuilder} 
-                    width="500" 
-                    height="400">
-                   </iframe>
-                </div>
-
-            </div > 
-            
+  return (
+    <section className="container" id="contacts">
+      <div className={styles.contactsContentGrid}>
+        <div className={styles.side}>
+          <h2>Контакты</h2>
+          <a href={addressLink} className={styles.socials}>
+            <p className={styles.socialTitle}>Адрес</p>
+            <p>{address}</p>
+          </a>
+          <a href={`tel:${phone}`} className={styles.socials}>
+            <p className={styles.socialTitle}>Телефон</p>
+            <p>{phone}</p>
+          </a>
+          <a href={`mailto:${email}`} className={styles.socials}>
+            <p className={styles.socialTitle}>Email</p>
+            <p>{email}</p>
+          </a>
+          <div className={styles.socials}>
+            <p className={styles.socialTitle}>Соцсети</p>
+            <div className={styles.links}>
+              {/* <Link href="https://m.vk.com/tridsat_dva" target="_blank">
+                <Image
+                  className={styles.icon}
+                  src="/svg/vk.svg"
+                  alt="VK"
+                  width={40}
+                  height={40}
+                />
+              </Link> */}
+              <Link href="https://wa.me/79029830005" target="_blank">
+                <Image
+                  className={styles.icon}
+                  src="/svg/wa.svg"
+                  alt="WhatsApp"
+                  width={40}
+                  height={40}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
-        </section>
-    );
+
+        <div className={styles.side}>
+          <div className={styles.map}>
+            <iframe src={addressBuilder} width="500" height="400"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
